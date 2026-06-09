bfe3db5548063cbca6cfce69b9e4bbf5533b5daa
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-06-09 13:18:56 +0530

    update dirzzle configration.

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env.DATABASE_URL)
 export default {
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
