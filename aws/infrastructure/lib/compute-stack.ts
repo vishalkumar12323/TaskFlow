ad6b2793ad82692420cb61561a84194ed11482d9
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-07-21 19:36:15 +0530

    extend the compute-stack with network-stack using props.

--- aws/infrastructure/lib/compute-stack.ts
+++ aws/infrastructure/lib/compute-stack.ts
@@ -1,8 +1,18 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
-export class InfrastructureStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface ComputeStackProps extends cdk.StackProps {
+    vpc: ec2.Vpc;
+}
+
+export class ComputeStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: ComputeStackProps) {
         super(scope, id, props);
+
+        // Use the VPC from NetworkStack
+        const vpc = props.vpc;
+
+        // Add your compute resources here using `vpc`
     }
 }
