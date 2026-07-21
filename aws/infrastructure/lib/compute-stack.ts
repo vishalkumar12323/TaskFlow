d450a7576cad94c6bf37774618e28ac805c073bc
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-07-21 22:50:24 +0530

    added security group and user data for linux.

--- aws/infrastructure/lib/compute-stack.ts
+++ aws/infrastructure/lib/compute-stack.ts
@@ -14,5 +14,26 @@ export class ComputeStack extends cdk.Stack {
         const vpc = props.vpc;
 
         // Add your compute resources here using `vpc`
+        const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+            vpc,
+            allowAllOutbound: true,
+            description: "Allows developers SSH access to EC2 Instance"
+        });
+
+        securityGroup.addIngressRule(
+            ec2.Peer.anyIpv4(),
+            ec2.Port.tcp(22),
+            "Allow SSH from anywhere"
+        );
+
+        const userData = ec2.UserData.forLinux();
+        userData.addCommands(
+            '#!/bin/bash',
+            'echo "User Data script started" > /var/log/user-data.log',
+            'sudo apt update',
+        );
+
+        // const amit = ec2.MachineImage.
+
     }
 }
