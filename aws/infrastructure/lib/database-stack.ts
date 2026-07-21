c529cc7e089bc0772a1156106aa2a39d883f16b2
Vishal Kumar <vishalninaniya8949info@gmail.com>
2026-07-21 19:35:14 +0530

    create database-stack

--- aws/infrastructure/lib/database-stack.ts
+++ aws/infrastructure/lib/database-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 
-export class InfrastructureStack extends cdk.Stack {
+export class DatabaseStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
     }
